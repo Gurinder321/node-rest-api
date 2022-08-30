9553f798d3293200fe64afc29106ea550c398dca
truptijogi7 <tjtrupti@gmail.com>
2022-08-30 20:53:05 +0530

    add jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post('/register', async (req, res) => {
@@ -8,17 +9,24 @@ router.post('/register', async (req, res) => {
     //generate new password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const userData = {
+      email: req.body.email,
+      password: hashedPassword
+  }
+    const token = jwt.sign(userData, secretKey);
     //create new user
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: hashedPassword,
+      password: hashedPassword 
     });
-
+    
     //save user and respond
-    const user = await newUser.save();
-    res.status(200).json(user);
+    const saveduser = await newUser.save();
+    const user = saveduser.toJSON();
+    user.token = token;
+    res.status(201).send(user);
   } catch (err) {
     res.status(500).json(err);
   }
